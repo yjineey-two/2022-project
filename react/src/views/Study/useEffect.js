bd9f83f20b2685f46ef2e1347ffa15cd6ee4ed53
jyang <did3706@naver.com>
2022-08-23 00:12:35 +0900

    useEffect 상단 여백 추가

--- react/src/views/Study/useEffect.js
+++ react/src/views/Study/useEffect.js
@@ -15,13 +15,17 @@ function HookPage() {
       <IndexHeader />
       <div className="section profile-content">
       <Container>
+        <div>　</div>
+        <div>　</div>
         {/* false 이면 재렌더링이 이뤄지지 않는다, true 여야만 */}
         {showTimer && <Timer />}
-        <button onClick={()=>{
-          setShowTimer(!showTimer)
-        }}> 
-          Toogle Timer
-        </button>
+        <div style={{ fontSize:'30px', fontWeight: 'bold' }}>
+          <button onClick={()=>{
+            setShowTimer(!showTimer)
+          }}> 
+            Toogle Timer
+          </button>
+        </div>
       </Container>
       </div>
       <IndexFooter />
